8e516c2ca426478fe0805340d40fd93bc4b7e213
Aaron Iglesias <aaron.iglesias@gmail.com>
2017-08-24 18:33:53 -0700

    feat: add print vector<int> and vector< vector<int> > functionality

--- src/print.h
+++ src/print.h
@@ -1,6 +1,30 @@
 #include <vector>
 
+void print(const int &n) {
+  std::cout << n << '\n';
+}
+
+void print(const double &n) {
+  std::cout << n << '\n';
+}
+
+void print() {
+  std::cout << '\n';
+}
+
 void print(const std::vector<double> &v) {
+  if(v.empty()) {
+    std::cout << "[]" << '\n';
+    return;
+  }
+  std::cout << '[';
+  for(int i = 0; i < v.size() - 1; ++i) {
+    std::cout << v[i] << ", ";
+  }
+  std::cout << v.back() << ']' << '\n';
+}
+
+void print(const std::vector<int> &v) {
   if(v.empty()) {
     std::cout << "[]" << '\n';
     return;
@@ -24,10 +48,14 @@ void print(const std::vector< std::vector<double> > &v) {
   std::cout << ']' << '\n';
 }
 
-void print(const double &n) {
-  std::cout << n << '\n';
-}
-
-void print() {
-  std::cout << '\n';
+void print(const std::vector< std::vector<int> > &v) {
+  if(v.empty()) {
+    std::cout << "[[]]" << '\n';
+    return;
+  }
+  std::cout << '[';
+  for(int i = 0; i < v.size() - 1; ++i) 
+    print(v[i]);
+  print(v.back());
+  std::cout << ']' << '\n';
 }
